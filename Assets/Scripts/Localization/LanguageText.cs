9e5a5e367b522991d18d0916c8f9476ab3c075d2
chanlinna <chanlinnalin@gmail.com>
2026-03-09 21:43:28 +0700

    create fotn size variable to adjust font size

--- Assets/Scripts/Localization/LanguageText.cs
+++ Assets/Scripts/Localization/LanguageText.cs
@@ -4,6 +4,8 @@ using UnityEngine.UI;
 public class LanguageText : MonoBehaviour
 {
     public string key;
+    public int englishFontSize = 40;
+    public int khmerFontSize = 48;
     private Text text;
 
     void Start()
@@ -16,5 +18,14 @@ public class LanguageText : MonoBehaviour
     {
         text.text = LanguageManager.instance.GetText(key);
         text.font = LanguageManager.instance.GetFont();
+
+        if (LanguageManager.instance.currentLanguage == LanguageManager.Language.Khmer)
+        {
+            text.fontSize = khmerFontSize;
+        }
+        else
+        {
+            text.fontSize = englishFontSize;
+        }
     }
 }
