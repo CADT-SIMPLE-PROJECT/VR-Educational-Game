912fd30a255fff19ef9b41a087ff23e8dbe326d9
chanlinna <chanlinnalin@gmail.com>
2026-03-30 20:28:13 +0700

    add ui canva when select animal

--- Assets/Scripts/UI/MenuController.cs
+++ Assets/Scripts/UI/MenuController.cs
@@ -8,9 +8,13 @@ namespace UI
         public GameObject mainMenuPanel;
         public GameObject settingsPanel;
         public GameObject openingTextPanel;
+        public GameObject animalDetailPanel;
 
         [Header("Locomotion Objects")]
         public GameObject locomotionSystem;
+
+        [Header("Opening Text Objects")]
+        public GameObject[] openingTexts;
 
         void Start()
         {
@@ -20,6 +24,7 @@ namespace UI
             // Show correct panels
             if (mainMenuPanel != null) mainMenuPanel.SetActive(true);
             if (openingTextPanel != null) openingTextPanel.SetActive(false);
+            UpdateOpeningText();
         }
 
         public void StartGame()
@@ -55,6 +60,66 @@ namespace UI
 
             // Hide UI (optional)
             gameObject.SetActive(false);
+        }
+
+        void UpdateOpeningText()
+        {
+            // Hide all texts first
+            foreach (GameObject text in openingTexts)
+            {
+                text.SetActive(false);
+            }
+
+            switch (currentStep)
+            {
+                case 0:
+                    openingTexts[0].SetActive(true); // Text 1
+                    break;
+
+                case 1:
+                    openingTexts[1].SetActive(true); // Text 2
+                    openingTexts[2].SetActive(true); // Text 3
+                    break;
+
+                case 2:
+                    openingTexts[3].SetActive(true); // Text 4
+                    openingTexts[4].SetActive(true); // Text 5
+                    openingTexts[5].SetActive(true); // Text 6
+                    break;
+
+                case 3:
+                    openingTexts[6].SetActive(true); // Text 7
+                    openingTexts[7].SetActive(true); // Text 8
+                    break;
+
+                default:
+                    SkipIntro(); // go to game
+                    break;
+            }
+        }
+
+        private int currentStep = 0;
+        public void NextOpeningText()
+        {
+            currentStep++;
+
+            UpdateOpeningText();
+        }
+
+        public void OpenAnimalDetailPanel()
+        {
+            if (animalDetailPanel != null)
+            {
+                animalDetailPanel.SetActive(true);
+            }
+        }
+
+        public void CloseAnimalDetailPanel()
+        {
+            if (animalDetailPanel != null)
+            {
+                animalDetailPanel.SetActive(false);
+            }
         }
     }
 }
